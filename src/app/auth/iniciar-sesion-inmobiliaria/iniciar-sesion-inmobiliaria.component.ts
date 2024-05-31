31f8fb4c19728dc9b6b2af76d4ab8ba34c162eb2
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2024-05-31 17:19:38 -0500

    IJIHU-60 Iniciar Sesion Inmobiliaria Frontend

--- src/app/auth/iniciar-sesion-inmobiliaria/iniciar-sesion-inmobiliaria.component.ts
+++ src/app/auth/iniciar-sesion-inmobiliaria/iniciar-sesion-inmobiliaria.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-iniciar-sesion-inmobiliaria',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './iniciar-sesion-inmobiliaria.component.html',
   styleUrl: './iniciar-sesion-inmobiliaria.component.css'
 })
